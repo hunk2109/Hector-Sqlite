a48c42b6de936b518f98bc46f9eb45c6e750c4d3
Hector Jose Taveras Rodriguez <hectojo_2109@hotmail.com>
2017-07-24 02:28:19 -0400

    Update 0.9

--- crud/Form1.Designer.cs
+++ crud/Form1.Designer.cs
@@ -32,7 +32,6 @@
             this.txtbuscar = new System.Windows.Forms.TextBox();
             this.btnbuscar = new System.Windows.Forms.Button();
             this.btncrear = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.cmbtablas = new System.Windows.Forms.ComboBox();
             this.txtcondiocional = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -89,18 +88,6 @@
             this.btncrear.Text = "crear";
             this.btncrear.UseVisualStyleBackColor = true;
             this.btncrear.Click += new System.EventHandler(this.btncrear_Click);
-            // 
-            // button1
-            // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button1.AutoSize = true;
-            this.button1.Location = new System.Drawing.Point(81, 397);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Prueba";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // cmbtablas
             // 
@@ -244,7 +231,6 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtcondiocional);
             this.Controls.Add(this.cmbtablas);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.btncrear);
             this.Controls.Add(this.btnbuscar);
             this.Controls.Add(this.txtbuscar);
@@ -264,7 +250,6 @@
         private System.Windows.Forms.TextBox txtbuscar;
         private System.Windows.Forms.Button btnbuscar;
         private System.Windows.Forms.Button btncrear;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox cmbtablas;
         private System.Windows.Forms.TextBox txtcondiocional;
         private System.Windows.Forms.Label label1;
